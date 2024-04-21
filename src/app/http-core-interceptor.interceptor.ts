09bdbf2851802c73cf8d194ea83a8ba50209ea6e
aquaperu <166466919+aquaperu@users.noreply.github.com>
2024-04-21 05:22:29 +0000

    comentando las formas en lo que trabaja el cors

--- src/app/http-core-interceptor.interceptor.ts
+++ src/app/http-core-interceptor.interceptor.ts
@@ -2,16 +2,38 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
 export const httpCoreInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
   const authToken = 'YOUR_AUTH_TOKEN_HERE';
+  // interceptor que habilita el cors en el cliente de donde está corriendo , tambien debe estar registrado en el servidor
+  //la otra opcion es crear un archivo proxy.config.js , poner el siguiente contenido
+  /**
+   * const PROXY_HOST = 'https://miniature-space-zebra-7v7j9qg6xrjx266g-4200.app.github.dev/';
+      const PROXY_CONFIG = [
+    {
+        context:['./'],
+        target:PROXY_HOST,
+        secure:false
+    }
+];
+module.exports = PROXY_CONFIG
 
+seguidamente ponerlo en angular.json
+en la seccion  "serve": {
+          "builder": "@angular-devkit/build-angular:dev-server",
+          "options": {
+            "proxyConfig": "proxy.conf.js"
+          },
+
+   */
   // Clone the request and add the authorization header
   const authReq = req.clone({
     setHeaders: {
       'Content-Type':'application/json',
-      'Access-Control-Allow-Origin':'*',
-      'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type,Accept',
+      
+      'Access-Control-Allow-Origin': 'https://miniature-space-zebra-7v7j9qg6xrjx266g-4200.app.github.dev',
+     
       Authorization: `Bearer ${authToken}`
     }
   });
+  console.log({"llamando desde el interceptor":authToken})
 
   // Pass the cloned request with the updated header to the next handler
   return next(authReq).pipe(
